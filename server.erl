861a6eca0cc05fcd852b8258f48159664ebc7083
Santaporo <dinopasalic1999@gmail.com>
2020-02-20 20:00:45 +0100

    channel join

--- server.erl
+++ server.erl
@@ -8,7 +8,12 @@ start(ServerAtom) ->
     % - Spawn a new process which waits for a message, handles it, then loops infinitely
     % - Register this process to ServerAtom
     % - Return the process ID
-    not_implemented.
+    catch(genserver:start(ServerAtom, [], fun handle/2)).
+
+handle(State,Data) ->io:fwrite("~p~n~p~n", [state, Data]),
+    {reply, ok, state}.
+    
+    
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
