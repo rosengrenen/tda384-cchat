e80d517f028812dd70223b50312dc69a18d65dd9
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-02-25 16:24:17 +0100

    Fixed stuffs, tests no work

--- client.erl
+++ client.erl
@@ -4,16 +4,22 @@
 
 % This record defines the structure of the state of a client.
 % Add whatever other fields you need.
--record(client_st,
-	{gui, % atom of the GUI process
-	 nick, % nick/username of the client
-	 server}). % atom of the chat server
+-record(
+  client_st,
+  {
+    % atom of the GUI process
+    gui,
+    % nick/username of the client
+    nick,
+    server
+  }
+).
 
+% atom of the chat server
 % Return an initial state record. This is called from GUI.
 % Do not change the signature of this function.
 initial_state(Nick, GUIAtom, ServerAtom) ->
-    #client_st{gui = GUIAtom, nick = Nick,
-	       server = ServerAtom}.
+  #client_st{gui = GUIAtom, nick = Nick, server = ServerAtom}.
 
 % handle/2 handles each kind of request from GUI
 % Parameters:
@@ -22,67 +28,51 @@ initial_state(Nick, GUIAtom, ServerAtom) ->
 % Must return a tuple {reply, Data, NewState}, where:
 %   - Data is what is sent to GUI, either the atom `ok` or a tuple {error, Atom, "Error message"}
 %   - NewState is the updated state of the client
-
 % Join channel
 handle(St, {join, Channel}) ->
-    % TODO: Implement this function
-    Result = genserver:request(St#client_st.server,
-			       {join, Channel, St#client_st.nick}),
-    case Result of
-      ok -> {reply, ok, St};
-      user_already_joined ->
-	  {reply,
-	   {error, user_already_joined,
-	    "you are already in this channel"},
-	   St}
-    end;
+  % TODO: Implement this function
+  case catch genserver:request(St#client_st.server, {join, Channel, self()}) of
+    ok -> {reply, ok, St};
+    user_already_joined ->
+      {reply, {error, user_already_joined, "you are already in this channel"}, St};
+    {'EXIT', _} -> {reply, {error, server_not_reached, "Server could not be reached"}, St}
+  end;
 %{reply, {error, not_implemented, "join not implemented"}, St} ;
 % Leave channel
 handle(St, {leave, Channel}) ->
-    % TODO: Implement this function
-    Result = genserver:request(list_to_atom(Channel),
-			       {leave, St#client_st.nick}),
-    case Result of
-      ok -> {reply, ok, St};
-      user_not_joined ->
-	  {reply,
-	   {error, user_not_joined, "User is not in this channel"},
-	   St}
-    end;
+  % TODO: Implement this function
+  case catch genserver:request(list_to_atom(Channel), {leave, self()}) of
+    ok -> {reply, ok, St};
+    user_not_joined -> {reply, {error, user_not_joined, "User is not in this channel"}, St};
+    {'EXIT', _} -> {reply, {error, server_not_reached, "Server could not be reached"}, St}
+  end;
 %{reply, {error, not_implemented, "leave not implemented"}, St} ;
 % Sending message (from GUI, to channel)
 handle(St, {message_send, Channel, Msg}) ->
-    % TODO: Implement this function
-    Result = genserver:request(list_to_atom(Channel),
-			       {message_send, St#client_st.nick, Msg}),
-    case Result of
-      ok -> {reply, ok, St};
-      user_not_joined ->
-	  {reply, {error, user_not_joined, "Bla"}, St}
-    end;
+  % TODO: Implement this function
+  case catch genserver:request(list_to_atom(Channel), {message_send, self(), St#client_st.nick, Msg}) of
+    ok -> {reply, ok, St};
+    user_not_joined -> {reply, {error, user_not_joined, "Bla"}, St};
+    {'EXIT', _} -> {reply, {error, server_not_reached, "Server could not be reached"}, St}
+  end;
 %{reply, {error, not_implemented, "message sending not implemented"}, St} ;
 % This case is only relevant for the distinction assignment!
 % Change nick (no check, local only)
 handle(St, {nick, NewNick}) ->
-    {reply, ok, St#client_st{nick = NewNick}};
+  io:fwrite("CHANGING NICK!!!~n", []),
+  {reply, ok, St#client_st{nick = NewNick}};
 % ---------------------------------------------------------------------------
 % The cases below do not need to be changed...
 % But you should understand how they work!
 % Get current nick
 handle(St, whoami) -> {reply, St#client_st.nick, St};
 % Incoming message (from channel, to GUI)
-handle(St = #client_st{gui = GUI},
-       {message_receive, Channel, Nick, Msg}) ->
-    gen_server:call(GUI,
-		    {message_receive, Channel, Nick ++ "> " ++ Msg}),
-    {reply, ok, St};
+handle(St = #client_st{gui = GUI}, {message_receive, Channel, Nick, Msg}) ->
+  gen_server:call(GUI, {message_receive, Channel, Nick ++ "> " ++ Msg}),
+  {reply, ok, St};
 % Quit client via GUI
 handle(St, quit) ->
-    % Any cleanup should happen here, but this is optional
-    {reply, ok, St};
+  % Any cleanup should happen here, but this is optional
+  {reply, ok, St};
 % Catch-all for any unhandled requests
-handle(St, Data) ->
-    {reply,
-     {error, not_implemented,
-      "Client does not handle this command"},
-     St}.
+handle(St, Data) -> {reply, {error, not_implemented, "Client does not handle this command"}, St}.
--- server.erl
+++ server.erl
@@ -4,75 +4,78 @@
 
 % This record defines the structure of the state of a client.
 % Add whatever other fields you need.
--record(channel_st,
-	{id, % channel id
-	 clients}). %clients in the server
+-record(
+  channel_st,
+  {
+    % channel id
+    id,
+    clients
+  }
+).
 
-initial_state(Id, Clients) ->
-    #channel_st{id = Id, clients = Clients}.
+%clients in the server
+initial_state(Id, Clients) -> #channel_st{id = Id, clients = Clients}.
 
 % Start a new server process with the given name
 % Do not change the signature of this function.
 start(ServerAtom) ->
-    % TODO Implement function
-    % - Spawn a new process which waits for a message, handles it, then loops infinitely
-    % - Register this process to ServerAtom
-    % - Return the process ID
-    catch genserver:start(ServerAtom, [], fun handle/2).
+  % TODO Implement function
+  % - Spawn a new process which waits for a message, handles it, then loops infinitely
+  % - Register this process to ServerAtom
+  % - Return the process ID
+  catch genserver:start(ServerAtom, [], fun handle/2).
 
 handle(State, {join, Channel, Client}) ->
-    io:fwrite("~p~n~p~n", [State, {join, Channel, Client}]),
-    case lists:member(Channel, State) of
-      true ->
-	  Result = genserver:request(list_to_atom(Channel),
-				     {join, Client}),
-	  {reply, Result, State};
-      false ->
-	  genserver:start(list_to_atom(Channel),
-			  initial_state(Channel, [Client]), fun channel/2),
-	  {reply, ok, [Channel | State]}
-    end.
+  case lists:member(Channel, State) of
+    true ->
+      Result = genserver:request(list_to_atom(Channel), {join, Client}),
+      {reply, Result, State};
+    false ->
+      genserver:start(list_to_atom(Channel), initial_state(Channel, [Client]), fun channel/2),
+      {reply, ok, [Channel | State]}
+  end;
+handle(State, {exit, _}) -> 
+  lists:foreach(fun (Channel) -> genserver:stop(list_to_atom(Channel)) end, State),
+  {reply, ok, State}.
 
 channel(State, {join, Client}) ->
-    io:fwrite("[Channel], ~p,~p~n", [State, Client]),
-    case lists:member(Client, State#channel_st.clients) of
-      true -> {reply, user_already_joined, State};
-      false ->
-	  {reply, ok,
-	   State#channel_st{clients =
-				[Client | State#channel_st.clients]}}
-    end;
+  case lists:member(Client, State#channel_st.clients) of
+    true -> {reply, user_already_joined, State};
+    false -> {reply, ok, State#channel_st{clients = [Client | State#channel_st.clients]}}
+  end;
 channel(State, {leave, Client}) ->
-    io:fwrite("[Channel], ~p,~p~n", [State, Client]),
-    case lists:member(Client, State#channel_st.clients) of
-      true ->
-	  {reply, ok,
-	   State#channel_st{clients =
-				lists:delete(Client,
-					     State#channel_st.clients)}};
-      false -> {reply, user_not_joined, State}
-    end;
-channel(State, {message_send, Client, Message}) ->
-    io:fwrite("[Channel], ~p,~p~n", [State, Client]),
-    case lists:member(Client, State#channel_st.clients) of
-      true ->
-	  lists:foreach(fun (Pid) ->
-				if Pid == Client -> skip;
-				   true ->
-				       genserver:request(list_to_atom(Pid),
-							 {message_receive,
-							  State#channel_st.id,
-							  Client, Message})
-				end
-			end,
-			State#channel_st.clients),
-	  {reply, ok, State};
-      false -> {reply, user_not_joined, State}
-    end.
+  case lists:member(Client, State#channel_st.clients) of
+    true -> {reply, ok, State#channel_st{clients = lists:delete(Client, State#channel_st.clients)}};
+    false -> {reply, user_not_joined, State}
+  end;
+channel(State, {message_send, Client, Nick, Message}) ->
+  case lists:member(Client, State#channel_st.clients) of
+    true ->
+      spawn(
+        fun
+          () ->
+            lists:foreach(
+              fun
+                (Pid) ->
+                  if
+                    Pid == Client -> skip;
+                    true ->
+                      genserver:request(Pid, {message_receive, State#channel_st.id, Nick, Message})
+                  end
+              end,
+              State#channel_st.clients
+            )
+        end
+      ),
+      {reply, ok, State};
+    false -> {reply, user_not_joined, State}
+  end.
 
 % Stop the server process registered to the given name,
 % together with any other associated processes
 stop(ServerAtom) ->
-    % TODO Implement function
-    % Return ok
-    not_implemented.
+  % TODO Implement function
+  % Return ok
+  genserver:request(ServerAtom, {exit, {}}),
+  genserver:stop(ServerAtom),
+  ok.
