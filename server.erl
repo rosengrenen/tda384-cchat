f9e19add7bf379025881ac95f12856e3c243336d
Rasmus Rosengren <rasmus.rosengren@pm.me>
2020-02-25 19:57:41 +0100

    Finalized code

--- server.erl
+++ server.erl
@@ -6,10 +6,13 @@
 -record(channel_state, {name, clients}).
 -record(client, {pid, nick}).
 
+% Create new server state
 server_state() -> #server_state{clients = [], channels = []}.
 
+% Create new channel state
 channel_state(Name) -> #channel_state{name = Name, clients = []}.
 
+% Create new client record
 client(Pid, Nick) -> #client{pid = Pid, nick = Nick}.
 
 % Start a new server process with the given name
@@ -21,62 +24,11 @@ start(ServerAtom) ->
   % - Return the process ID
   catch genserver:start(ServerAtom, server_state(), fun handle/2).
 
-nick_exists(_, []) -> false;
-nick_exists(Nick, [Client | RestOfClients]) ->
-  case Nick == Client#client.nick of
-    true -> true;
-    false -> nick_exists(Nick, RestOfClients)
-  end.
-
-pid_exists(_, []) -> false;
-pid_exists(Pid, [Client | RestOfClients]) ->
-  case Pid == Client#client.pid of
-    true -> true;
-    false -> nick_exists(Pid, RestOfClients)
-  end.
-
-channel_exists(Channel, Channels) -> lists:member(Channel, Channels).
-
-update_client_nick(_, _, []) -> [];
-update_client_nick(NewNick, ClientPid, [Client | RestOfClients]) ->
-  if
-    ClientPid == Client#client.pid -> [Client#client{nick = NewNick} | RestOfClients];
-    true -> [Client | update_client_nick(NewNick, ClientPid, RestOfClients)]
-  end.
-
-validate_client(State, RequestInput = {_, ClientPid, ClientNick}) ->
-  case pid_exists(ClientPid, State#server_state.clients) of
-    true -> check_channel(State, RequestInput);
-    false ->
-      case nick_exists(ClientNick, State#server_state.clients) of
-        true -> {nick_taken, State};
-        false ->
-          check_channel(
-            State#server_state{
-              clients = [client(ClientPid, ClientNick) | State#server_state.clients]
-            },
-            RequestInput
-          )
-      end
-  end.
-
-check_channel(State, RequestInput = {ChannelName, _, _}) ->
-  case channel_exists(ChannelName, State#server_state.channels) of
-    true -> join_channel(State, RequestInput);
-    false ->
-      genserver:start(list_to_atom(ChannelName), channel_state(ChannelName), fun channel/2),
-      join_channel(
-        State#server_state{channels = [ChannelName | State#server_state.channels]},
-        RequestInput
-      )
-  end.
-
-join_channel(State, {ChannelName, ClientPid, _}) ->
-  {genserver:request(list_to_atom(ChannelName), {join, ClientPid}), State}.
-
+% Handle a join request
 handle(State, {join, ChannelName, ClientPid, ClientNick}) ->
   {Result, NewState} = validate_client(State, {ChannelName, ClientPid, ClientNick}),
   {reply, Result, NewState};
+% Handle a nick change request
 handle(State, {nick, ClientPid, NewClientNick}) ->
   case lists:member(client(ClientPid, NewClientNick), State#server_state.clients) of
     true -> {reply, ok, State};
@@ -93,6 +45,7 @@ handle(State, {nick, ClientPid, NewClientNick}) ->
           }
       end
   end;
+% Handle a server shut down request
 handle(State, {exit, _}) ->
   lists:foreach(
     fun (Channel) -> genserver:stop(list_to_atom(Channel)) end,
@@ -100,38 +53,33 @@ handle(State, {exit, _}) ->
   ),
   {reply, ok, State}.
 
-channel(State, {join, Client}) ->
-  case lists:member(Client, State#channel_state.clients) of
+% Handle a channel join request
+channel(State, {join, ClientPid}) ->
+  case lists:member(ClientPid, State#channel_state.clients) of
     true -> {reply, user_already_joined, State};
-    false -> {reply, ok, State#channel_state{clients = [Client | State#channel_state.clients]}}
+    false -> {reply, ok, State#channel_state{clients = [ClientPid | State#channel_state.clients]}}
   end;
-channel(State, {leave, Client}) ->
-  case lists:member(Client, State#channel_state.clients) of
+% Handle a channel leave request
+channel(State, {leave, ClientPid}) ->
+  case lists:member(ClientPid, State#channel_state.clients) of
     true ->
-      {reply, ok, State#channel_state{clients = lists:delete(Client, State#channel_state.clients)}};
+      {
+        reply,
+        ok,
+        State#channel_state{clients = lists:delete(ClientPid, State#channel_state.clients)}
+      };
     false -> {reply, user_not_joined, State}
   end;
-channel(State, {message_send, Client, Nick, Message}) ->
-  case lists:member(Client, State#channel_state.clients) of
+% Handle a channel message request
+channel(State, {message_send, ClientPid, ClientNick, Message}) ->
+  case lists:member(ClientPid, State#channel_state.clients) of
     true ->
-      spawn(
-        fun
-          () ->
-            lists:foreach(
-              fun
-                (Pid) ->
-                  if
-                    Pid == Client -> skip;
-                    true ->
-                      genserver:request(
-                        Pid,
-                        {message_receive, State#channel_state.name, Nick, Message}
-                      )
-                  end
-              end,
-              State#channel_state.clients
-            )
-        end
+      broadcast_message(
+        State#channel_state.name,
+        ClientPid,
+        ClientNick,
+        Message,
+        State#channel_state.clients
       ),
       {reply, ok, State};
     false -> {reply, user_not_joined, State}
@@ -145,3 +93,82 @@ stop(ServerAtom) ->
   genserver:request(ServerAtom, {exit, {}}),
   genserver:stop(ServerAtom),
   ok.
+
+% Check if a client with a given nick exists in a list of clients
+nick_exists(_, []) -> false;
+nick_exists(Nick, [Client | RestOfClients]) ->
+  case Nick == Client#client.nick of
+    true -> true;
+    false -> nick_exists(Nick, RestOfClients)
+  end.
+
+% Check if a client with a given pid exists in a list of clients
+pid_exists(_, []) -> false;
+pid_exists(Pid, [Client | RestOfClients]) ->
+  case Pid == Client#client.pid of
+    true -> true;
+    false -> nick_exists(Pid, RestOfClients)
+  end.
+
+% Check if a channel exists in a list of channels
+channel_exists(Channel, Channels) -> lists:member(Channel, Channels).
+
+% Update a client to use a new nick
+update_client_nick(_, _, []) -> [];
+update_client_nick(NewNick, ClientPid, [Client | RestOfClients]) ->
+  if
+    ClientPid == Client#client.pid -> [Client#client{nick = NewNick} | RestOfClients];
+    true -> [Client | update_client_nick(NewNick, ClientPid, RestOfClients)]
+  end.
+
+% Check that a client trying to join a channel has a unique nick
+validate_client(State, RequestInput = {_, ClientPid, ClientNick}) ->
+  case pid_exists(ClientPid, State#server_state.clients) of
+    true -> check_channel(State, RequestInput);
+    false ->
+      case nick_exists(ClientNick, State#server_state.clients) of
+        true -> {nick_taken, State};
+        false ->
+          check_channel(
+            State#server_state{
+              clients = [client(ClientPid, ClientNick) | State#server_state.clients]
+            },
+            RequestInput
+          )
+      end
+  end.
+
+% Check if a channel exists, else create it
+check_channel(State, RequestInput = {ChannelName, _, _}) ->
+  case channel_exists(ChannelName, State#server_state.channels) of
+    true -> join_channel(State, RequestInput);
+    false ->
+      genserver:start(list_to_atom(ChannelName), channel_state(ChannelName), fun channel/2),
+      join_channel(
+        State#server_state{channels = [ChannelName | State#server_state.channels]},
+        RequestInput
+      )
+  end.
+
+% Join a channel
+join_channel(State, {ChannelName, ClientPid, _}) ->
+  {genserver:request(list_to_atom(ChannelName), {join, ClientPid}), State}.
+
+% Broadcast a message to all clients in a channel
+broadcast_message(ChannelName, ClientPid, ClientNick, Message, Clients) ->
+  spawn(
+    fun
+      () ->
+        [
+          broadcast_message_to_client(Pid, ChannelName, ClientPid, ClientNick, Message)
+          || Pid <- Clients
+        ]
+    end
+  ).
+
+% Broadcast a message to a client in a server
+broadcast_message_to_client(Pid, ChannelName, ClientPid, ClientNick, Message) ->
+  if
+    Pid == ClientPid -> skip;
+    true -> genserver:request(Pid, {message_receive, ChannelName, ClientNick, Message})
+  end.
